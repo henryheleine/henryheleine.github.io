11fe21592129806e371de3a8de93482b7c420c21
Henry Heleine <henryheleine86@gmail.com>
2023-06-26 09:52:26 -0400

    add simple post message for consent

--- index.js
+++ index.js
@@ -74,6 +74,13 @@ app.get("/mobile/2.0/article/12060079", function(req, res) {
     })
 })
 
+app.get("/mobile/2.0/article/12345", function(req, res) {
+    fs.readFile("12345.json", function(error, data) {
+        res.writeHead(200, {"Content-Type":"application/json"})
+        res.end(data)
+    })
+})
+
 app.get("/reader-comments/p/asset/readcomments/11888153", function(req, res) {
     fs.readFile("firework-comments.json", function(error, data) {
         res.writeHead(200, {"Content-Type":"application/json"})
