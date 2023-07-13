78b45adb105aff9292395023f227a239ee00086a
Henry Heleine <henryheleine86@gmail.com>
2023-07-13 10:14:35 -0400

    xp mod target examples

--- index.js
+++ index.js
@@ -81,6 +81,13 @@ app.get("/mobile/2.0/article/12345", function(req, res) {
     })
 })
 
+app.get("/mobile/2.0/article/12292325", function(req, res) {
+    fs.readFile("12292325.json", function(error, data) {
+        res.writeHead(200, {"Content-Type":"application/json"})
+        res.end(data)
+    })
+})
+
 app.get("/reader-comments/p/asset/readcomments/11888153", function(req, res) {
     fs.readFile("firework-comments.json", function(error, data) {
         res.writeHead(200, {"Content-Type":"application/json"})
