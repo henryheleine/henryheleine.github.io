a832379d41b167990330aef1130b6df7eda94163
Henry Heleine <henryheleine86@gmail.com>
2023-08-09 09:29:43 -0400

    add twitter embed article example

--- index.js
+++ index.js
@@ -102,6 +102,13 @@ app.get("/mobile/2.0/article/12279945", function(req, res) {
     })
 })
 
+app.get("/mobile/2.0/article/6172937", function(req, res) {
+    fs.readFile("6172937.json", function(error, data) {
+        res.writeHead(200, {"Content-Type":"application/json"})
+        res.end(data)
+    })
+})
+
 app.get("/reader-comments/p/asset/readcomments/11888153", function(req, res) {
     fs.readFile("firework-comments.json", function(error, data) {
         res.writeHead(200, {"Content-Type":"application/json"})
