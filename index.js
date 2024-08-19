55443cfe9c9951619ff2a6e8e73a9497a341bfe5
Henry Heleine <henryheleine86@gmail.com>
2024-08-19 12:44:25 -0400

    add ua url

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get("/", function(req,res) {
     res.type('html').send("<html><body><h1>Hello There!</h1></body></html>")
 })
 
+app.get("/ua", function(req,res) {
+    const userAgent = req.headers['user-agent']
+    res.writeHead(200, {"Content-Type":"text/html"})
+    res.end("{ \"userAgent\": \"" + userAgent + "\"}")
+})
+
 app.get("/enterprisebuild/v5.5", function(req, res) {
     res.send("<html><head><style>{height:100%;width:100%;}body{padding:36px;font-size:42px;}a{padding:40px;margin120px;display:block;background-color:#3333AA;color:#fff;font-size:80px;}p{margin:20px;}img{margin:26px;}</style></head><body><center><h1><a href='itms-services://?action=download-manifest&amp;url=https://render-4ezx.onrender.com/MailOnline.plist'>MailOnline Enterprise<BR>App v5.5 (550000000027)</a></h1><p>Tap the link above, after a short pause an alert will appear - tap Install:<br><img height='190px' width='412px' src='https://render-4ezx.onrender.com/alertimg.png'><br>There is no further response. Go to your home/springboard and see the app icon installing.</p><br>RELEASE NOTES: Google login is not supported in test environment but Facebook login should work as expected.<br><br><b>FIRST TIME: </b><br>You will need to 'trust' each enterprise build at least once. Go to Settings and then:</center><ul style='list-style-type: none;'><li> - General </li><li> - Profiles & Device Management </li><li> - Associated Newspapers Limited </li><li> - Trust. </li></ul></p></body></html>")
 })
