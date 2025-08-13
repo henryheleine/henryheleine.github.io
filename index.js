2e8798ef52e5e217a26ae893eace55c68e247bad
Henry Heleine <henryheleine86@gmail.com>
2025-08-13 10:47:44 -0400

    image upload post endpoint 10

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post("/upload", function(req,res) {
 })
 
 async function processUpload(res) {
-    res.writeHead(200, { "Content-Type": "text/plain", "Transfer-Encoding": "chunked"})
+    res.writeHead(200, { "Content-Type": "application/json", "Transfer-Encoding": "chunked"})
     for (var i = 0; i < 100; i++) {
         res.write("one update")
     }
